63b4eceecd9d2fe7b76fc90bda03a041517e8e2a
Aldo Stracquadanio <aldo.strac@gmail.com>
2013-06-10 01:15:20 +0100

    Reintroduced map!

--- app/scripts/directives/ms-map-widget.js
+++ app/scripts/directives/ms-map-widget.js
@@ -3,9 +3,26 @@
 angular.module('mapsheetApp')
   .directive('msMapWidget', function () {
     return {
-      template: '<div>map</div>',
+      templateUrl: 'views/map-widget.html',
+      scope: {},
       restrict: 'A',
-      link: function postLink(scope, element, attrs) {
+      link: function(scope, element, controller) {
+        scope.lat = 0;
+        scope.lon = 0;
+
+        if (!scope.map) {
+          scope.map = L.map('map').setView([51.505, -0.09], 13);
+          L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          }).addTo(scope.map);
+        }
+
+        scope.map.on('mousemove', function(evt) {
+          scope.$apply(function() {
+              scope.lat = evt.latlng.lat;
+              scope.lon = evt.latlng.lng;
+          });
+        });
       }
     };
   });
